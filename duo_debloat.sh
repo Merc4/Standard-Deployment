db87e3708c94676fc11fcd6cc9661517c9f9846e
Merc4 <Kai.Hsu1984@gmail.com>
2023-05-05 18:00:59 +0100

    Added Duo Postinstall Optimise

---
+++ duo_debloat.sh
@@ -0,0 +1,31 @@
+# Edge: pm uninstall --user 0 com.microsoft.emmx
+# Google Meet pm uninstall --user 0 com.google.android.apps.tachyon
+pm uninstall --user 0 com.amazon.kindle
+pm uninstall --user 0 com.android.chrome
+pm uninstall --user 0 com.azure.authenticator
+pm uninstall --user 0 com.gamepass
+pm uninstall --user 0 com.google.android.apps.docs
+pm uninstall --user 0 com.google.android.apps.maps
+pm uninstall --user 0 com.google.android.apps.photos
+pm uninstall --user 0 com.google.android.apps.tachyon
+pm uninstall --user 0 com.google.android.apps.youtube.music
+pm uninstall --user 0 com.google.android.gm
+pm uninstall --user 0 com.google.android.googlequicksearchbox
+pm uninstall --user 0 com.google.android.videos
+pm uninstall --user 0 com.google.android.youtube
+pm uninstall --user 0 com.linkedin.android
+pm uninstall --user 0 com.microsoft.amp.apps.bingnews
+pm uninstall --user 0 com.microsoft.bing
+pm uninstall --user 0 com.microsoft.emmx
+pm uninstall --user 0 com.microsoft.office.officehubrow
+pm uninstall --user 0 com.microsoft.office.officelens
+pm uninstall --user 0 com.microsoft.office.onenote
+pm uninstall --user 0 com.microsoft.office.outlook
+pm uninstall --user 0 com.microsoft.skydrive
+pm uninstall --user 0 com.microsoft.surface.tips
+pm uninstall --user 0 com.microsoft.surfaceaudio
+pm uninstall --user 0 com.microsoft.teams
+pm uninstall --user 0 com.microsoft.todos
+pm uninstall --user 0 com.microsoft.windowsintune.companyportal
+pm uninstall --user 0 com.spotify.music
+pm uninstall --user 0 com.zhiliaoapp.musically
